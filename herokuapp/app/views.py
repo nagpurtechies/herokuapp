17f9aba5732076d69d6acd3517bd034a73b8c0c2
Asad Dhamani <dhamaniasad@gmail.com>
2015-09-17 21:37:41 +0530

    Added simple template

--- herokuapp/app/views.py
+++ herokuapp/app/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
-
+from .models import Post
+from django.template import RequestContext, loader
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    latest_posts = Post.objects.order_by("-pub_date")
+    template = loader.get_template('index.html')
+    context = RequestContext(request, {
+        'latest_posts_list': latest_posts,
+    })
+    latest_posts = ', '.join([p.title for p in latest_posts])
+    return HttpResponse(template.render(context))
