f45cd4908d98f113662f301bfdb3755e54ffce91
Asad Dhamani <dhamaniasad@gmail.com>
2015-09-26 18:20:33 +0530

    Update create template

--- herokuapp/app/forms.py
+++ herokuapp/app/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class CreateForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=300)
-    text = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(label='Title', max_length=300, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    text = forms.CharField(label='Text', widget=forms.Textarea(attrs={'class': 'form-control'}))
